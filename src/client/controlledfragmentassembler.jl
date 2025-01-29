44fae4c08e8702927855a5453f8e4e9d9d3b5a04
Darryl Gamroth <dgamroth@rubus.ca>
2025-01-29 12:11:19 -0800

    Refactor fragment assemblers

--- src/client/controlledfragmentassembler.jl
+++ src/client/controlledfragmentassembler.jl
@@ -5,32 +5,31 @@ Represents a controlled fragment assembler that reassembles fragmented messages 
 
 # Constructor
 
-- `ControlledFragmentAssembler(on_fragment::T)`: Creates a new `ControlledFragmentAssembler` with the given controlled fragment handler.
+- `ControlledFragmentAssembler(fragment_handler::T)`: Creates a new `ControlledFragmentAssembler` with the given controlled fragment handler.
 """
-mutable struct ControlledFragmentAssembler{T,C,F<:AbstractControlledFragmentHandler} <: AbstractControlledFragmentHandler
-    on_fragment::T
-    clientd::C
-    fragment_handler::F
-end
+mutable struct ControlledFragmentAssembler{T<:AbstractControlledFragmentHandler} <: AbstractControlledFragmentHandler
+    fragment_handler::T
+    assembler::Ptr{aeron_controlled_fragment_assembler_t}
 
-on_fragment(f::ControlledFragmentAssembler) = f.on_fragment
-clientd(f::ControlledFragmentAssembler) = f.clientd
+    function ControlledFragmentAssembler(fragment_handler::T) where {T}
+        assembler = Ref{Ptr{aeron_controlled_fragment_assembler_t}}(C_NULL)
 
-function ControlledFragmentAssembler(fragment_handler::AbstractControlledFragmentHandler)
-    assembler = Ref{Ptr{aeron_controlled_fragment_assembler_t}}(C_NULL)
+        if aeron_controlled_fragment_assembler_create(assembler,
+            on_fragment_cfunction(fragment_handler),
+            Ref(fragment_handler)) < 0
+            throwerror()
+        end
 
-    if aeron_controlled_fragment_assembler_create(assembler,
-        on_fragment_cfunction(fragment_handler),
-        Ref(fragment_handler)) < 0
-        throwerror()
-    end
-
-    fa = ControlledFragmentAssembler(assembler[], fragment_handler) do clientd, buffer, header
-        action = aeron_controlled_fragment_assembler_handler(clientd, buffer, length(buffer), pointer(header))
-        return ControlledAction.T(Integer(action))
-    end
-
-    finalizer(fa) do fa
-        aeron_controlled_fragment_assembler_delete(fa.clientd)
+        finalizer(new{T}(fragment_handler, assembler[])) do f
+            aeron_controlled_fragment_assembler_delete(f.assembler)
+        end
     end
 end
+
+function (f::ControlledFragmentAssembler)(clientd, buffer, header)
+    action = aeron_controlled_fragment_assembler_handler(clientd, buffer, length(buffer), pointer(header))
+    return ControlledAction.T(Integer(action))
+end
+
+on_fragment(f::ControlledFragmentAssembler) = f
+clientd(f::ControlledFragmentAssembler) = f.assembler
--- src/client/fragmentassembler.jl
+++ src/client/fragmentassembler.jl
@@ -5,31 +5,30 @@ Represents a fragment assembler that reassembles fragmented messages and passes 
 
 # Constructor
 
-- `FragmentAssembler(on_fragment::T)`: Creates a new `FragmentAssembler` with the given fragment handler.
+- `FragmentAssembler(fragment_handler::T)`: Creates a new `FragmentAssembler` with the given fragment handler.
 """
-mutable struct FragmentAssembler{T,C,F<:AbstractFragmentHandler} <: AbstractFragmentHandler
-    on_fragment::T
-    clientd::C
-    fragment_handler::F
-end
+mutable struct FragmentAssembler{T<:AbstractFragmentHandler} <: AbstractFragmentHandler
+    fragment_handler::T
+    assembler::Ptr{aeron_fragment_assembler_t}
 
-on_fragment(f::FragmentAssembler) = f.on_fragment
-clientd(f::FragmentAssembler) = f.clientd
+    function FragmentAssembler(fragment_handler::T) where {T}
+        assembler = Ref{Ptr{aeron_fragment_assembler_t}}(C_NULL)
 
-function FragmentAssembler(fragment_handler::AbstractFragmentHandler)
-    assembler = Ref{Ptr{aeron_fragment_assembler_t}}(C_NULL)
+        if aeron_fragment_assembler_create(assembler,
+            on_fragment_cfunction(fragment_handler),
+            Ref(fragment_handler)) < 0
+            throwerror()
+        end
 
-    if aeron_fragment_assembler_create(assembler,
-        on_fragment_cfunction(fragment_handler),
-        Ref(fragment_handler)) < 0
-        throwerror()
-    end
-
-    fa = FragmentAssembler(assembler[], fragment_handler) do clientd, buffer, header
-        aeron_fragment_assembler_handler(clientd, buffer, length(buffer), pointer(header))
-    end
-
-    finalizer(fa) do fa
-        aeron_fragment_assembler_delete(fa.clientd)
+        finalizer(new{T}(fragment_handler, assembler[])) do f
+            aeron_fragment_assembler_delete(f.assembler)
+        end
     end
 end
+
+function (f::FragmentAssembler)(clientd, buffer, header)
+    aeron_fragment_assembler_handler(clientd, buffer, length(buffer), pointer(header))
+end
+
+on_fragment(f::FragmentAssembler) = f
+clientd(f::FragmentAssembler) = f.assembler
